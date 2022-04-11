da9ecaf08344f90d67dc1c5f14a51ecdd66394bb
Atte-Oskari-Rasanen <70641619+Atte-Oskari-Rasanen@users.noreply.github.com>
2022-04-11 23:56:53 +0200

    Update kivy_test.py
    made it so that the image can be dragged to a country and it stays there. made the countries transparent, organised as stacklayout instead of box

--- kivy_test.py
+++ kivy_test.py
@@ -7,59 +7,85 @@ from kivy.properties import BooleanProperty, ListProperty
 from kivy.animation import Animation
 from kivy.uix.image import Image, AsyncImage
 #https://realpython.com/mobile-app-kivy-python/#displaying-an-image
+from kivy.core.window import Window
+Window.clearcolor = (1, 1, 1, 1)
+
+
+
+#https://github.com/kivy-garden/draggable
+
+#layouts:https://kivy.org/doc/stable/guide/widgets.html
 kv = """
+
 <DragButton>:
 
     drag_rectangle: self.x, self.y, self.width, self.height
     drag_timeout: 10000000
     drag_distance: 10
     on_release: print(f'Drag Button {self.text} pressed')
+    name: 'Test game'
 
-BoxLayout:
-    BoxLayout:
-        orientation: 'vertical'
-
+StackLayout:
+    size_hint: .5, .5
+    StackLayout:
+        #orientation: 'vertical'
+        size_hint: .2, .2
         DragButton:
-            text: ' '
-            icon_size: 200, 200
             canvas:
                 Rectangle:
+                    pos: self.pos
+                    size: self.size
                     source: '/home/atte/Documents/Kivy_stuff/assets/ims/cow.png'
-                    size: self.icon_size
-                    #allow_stretch: True
-                    pos: self.pos[0] + self.width - self.icon_size[0], self.pos[1] + self.icon_size[1] / 2
 
-
-        # DragButton:
-        #     text: ''
-        #     #icon_size: 16, 16
-        #     canvas:
-        #         Rectangle:
-        #             source: '/home/atte/Documents/Kivy_stuff/assets/ims/fox.png'
-        #             size: self.icon_size
-        #             allow_stretch: True
-        #             pos: self.pos[0] + self.width - self.icon_size[0], self.pos[1] + self.icon_size[1] / 2
-        
         DragButton:
-            text: '3'
-    BoxLayout:
-        id: middle
-    BoxLayout:
-        id:right
-#        orientation: 'vertical'
-        Image:
-            id: remove_zone
-            source: '/home/atte/Documents/Kivy_stuff/assets/ims/spain.png'
+            canvas:
+                Rectangle:
+                    pos: self.pos
+                    size: self.size
+                    source: '/home/atte/Documents/Kivy_stuff/assets/ims/fox.png'
 
-            # canvas:
-            #     Rectangle:
-            #         source: '/home/atte/Documents/Kivy_stuff/assets/ims/spain.png'
-            #         size: self.icon_size
-            #         pos: self.pos[0] + self.width - self.icon_size[0], self.pos[1] + self.icon_size[1] / 2
+                # canvas:
+                #     Rectangle:
+                #         source: '/home/atte/Documents/Kivy_stuff/assets/ims/cow.png'
+                #         
+                #         size: self.icon_size
+                #         #allow_stretch: True
+                #         pos: self.pos[0] + self.width - self.icon_size[0], self.pos[1] + self.icon_size[1] / 2
+
 
         Image:
             id: remove_zone
             source: '/home/atte/Documents/Kivy_stuff/assets/ims/sweden.png'
+            pos_hint: {'left':1, 'top':1}
+            size_hint: 1, 1
+            pos: 100, 100
+            size: 200, 300
+            allow_stretch: True
+            keep_ratio: False
+            canvas.before:
+                Color:
+                    rgba: 1, 1, 1, 0.2    # blue
+                Rectangle:
+                    pos: self.pos
+                    size: self.size
+
+        Image:
+            id: remove_zone
+            source: '/home/atte/Documents/Kivy_stuff/assets/ims/spain.png'
+            pos: 100, 200
+            pos_hint: {'left':1, 'top':1}
+            
+            size_hint: 1, 1
+            size: 200, 300
+            allow_stretch: True
+            keep_ratio: False
+            canvas.before:
+                Color:
+                    rgba: 1, 1, 1, 0.2    # blue
+                Rectangle:
+                    pos: self.pos
+                    size: self.size
+
 
             # orientation: 'vertical'
             # Label:
@@ -75,6 +101,14 @@ BoxLayout:
             #     text: 'Remove Widget'
 """
 
+class ThirdScreen(Screen):
+    def enable_cropping(self):
+        print("\nThirdScreen:")
+        print(self.ids.main_image.pos)
+        print(self.ids.main_image.size)
+        print("\tAbsolute size=", self.ids.main_image.norm_image_size)
+        print("\tAbsolute pos_x=", self.ids.main_image.center_x - self.ids.main_image.norm_image_size[0] / 2.)
+        print("\tAbsolute pos_y=", self.ids.main_image.center_y - self.ids.main_image.norm_image_size[1] / 2.)
 
 class DragButton(DragBehavior, Button):
     dragging = BooleanProperty(False)
@@ -104,10 +138,11 @@ class DragButton(DragBehavior, Button):
             self.opacity = 1
             self.dragging = False
             if self.collide_widget(app.root.ids.remove_zone):  # refers to the image in kv string, removes the image
+                self.parent.
                 self.parent.remove_widget(self)
-            else:
-                anim = Animation(pos=self.original_pos, duration=1)
-                anim.start(self)
+            # else:
+            #     anim = Animation(pos=self.original_pos, duration=1)
+            #     anim.start(self)
         return super().on_touch_up(touch)
 
 
